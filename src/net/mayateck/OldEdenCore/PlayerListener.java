0d000caa1f49662f65c38c2b969d157f40046181
Matthew Struble <Matt@MayateckInnovations.net>
2013-08-09 11:03:32 -0700

    Now handles the Join/Quit messages.

--- src/net/mayateck/OldEdenCore/PlayerListener.java
+++ src/net/mayateck/OldEdenCore/PlayerListener.java
@@ -1,9 +1,11 @@
 package net.mayateck.OldEdenCore;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener{
 	private OldEdenCore plugin;
@@ -16,6 +18,7 @@ public class PlayerListener implements Listener{
 	public void onPlayerJoin(PlayerJoinEvent evt){
 		Player player = evt.getPlayer();
 		String pname = player.getName();
+		evt.setJoinMessage(AlertsHandler.alertSys+ChatColor.BLUE+pname+ChatColor.GRAY+" connected. Sync Completed.");
 		plugin.getLogger().info("Player "+pname+" connected.");
 		if (plugin.getConfig().contains("players."+pname)){
 			player.sendMessage(OldEdenCore.serverHead+"Welcome back, "+pname+".");
@@ -32,5 +35,12 @@ public class PlayerListener implements Listener{
 			plugin.saveConfig();
 			player.sendMessage(OldEdenCore.serverHead+"Complete! Welcome "+pname+"!");
 		}
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent evt){
+		Player player = evt.getPlayer();
+		String pname = player.getName();
+		evt.setQuitMessage(AlertsHandler.alertSys+ChatColor.BLUE+pname+ChatColor.GRAY+" disconnected.");
 	}
 }
