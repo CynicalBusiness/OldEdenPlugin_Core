14716da182b49baef2fad61a84af44b138848f26
Matthew Struble <Matt@MayateckInnovations.net>
2013-08-09 09:31:59 -0700

    Re-arranged Listener data.

--- src/net/mayateck/OldEdenCore/OldEdenCore.java
+++ src/net/mayateck/OldEdenCore/OldEdenCore.java
@@ -2,54 +2,52 @@ package net.mayateck.OldEdenCore;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.*;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.*;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class OldEdenCore extends JavaPlugin{
+public class OldEdenCore extends JavaPlugin implements Listener{
 	public static String version = "0.00.01";
 	public static boolean isConn = false;
-	public static String head = ChatColor.DARK_GRAY+"||"+ChatColor.AQUA+"Core"+ChatColor.DARK_GRAY+"|| ";
-	public Plugin basePlugin = this;
+	public static String head = ChatColor.DARK_GRAY+" || "+ChatColor.AQUA+"PLUGIN"+ChatColor.DARK_GRAY+" || ";
+	public static String serverHead = ChatColor.DARK_GRAY+" || "+ChatColor.GRAY+"SERVER"+ChatColor.DARK_GRAY+" || ";
+	Plugin plugin = this;
 	
 	@Override
 	public void onEnable(){
-		getLogger().info("#======# Old Eden Core v"+version+" #======#");
-		getLogger().info("Initializing Command Data...");
+		this.getLogger().info("#======# Old Eden Core v"+version+" #======#");
+		this.getLogger().info("Initializing Command Data...");
 			getCommand("money").setExecutor(new EconomyHandler(this));
 			getCommand("alert").setExecutor(new AlertsHandler(this));
-		getLogger().info("Requesting response from configuration...");
+			
+			new PlayerListener(this);
+		this.getLogger().info("Requesting response from configuration...");
 			this.saveDefaultConfig();
-		getLogger().info("Initialization complete.");
-		getLogger().info("#======================================#");
+		this.getLogger().info("Initialization complete.");
+		this.getLogger().info("#======================================#");
 	}
 	
 	@Override
 	public void onDisable(){
-		getLogger().info("#======# Old Eden Core v"+version+" #======#");
-		getLogger().info("Shutting down...");
+		this.getLogger().info("#======# Old Eden Core v"+version+" #======#");
+		this.getLogger().info("Shutting down...");
 		// TODO: Wrap-up.
-		getLogger().info("Shut-down complete.");
-		getLogger().info("#======================================#");
+		this.getLogger().info("Shut-down complete.");
+		this.getLogger().info("#======================================#");
 	}
 	
 	public boolean onCommand(CommandSender s, Command cmd, String l, String[] args){
 		if (cmd.getName().equalsIgnoreCase("eden")){
-			if (args[0].equalsIgnoreCase("reload") && s.hasPermission("eden.reload")){
+			if (args[0].equalsIgnoreCase("reload") && s.hasPermission("eden.reload") && args.length==1){
+				plugin.reloadConfig();
+				s.sendMessage(head+"Data files successfully reloaded.");
+				return true;
+			} else {
+				s.sendMessage(head+"The command was invalid or you lack sufficient permission.");
+				s.sendMessage(head+"Try '/eden help'.");
+				return true;
 			}
 		}
 		return false;
-	}
-	
-	public void onPlayerJoin(PlayerJoinEvent evt){
-		Player player = evt.getPlayer();
-		String name = player.getName();
-//		if (plyrs.contains(name)){
-			player.sendMessage("Welcome back, "+name+".");
-//		} else {
-//			player.sendMessage("Welcome to Old Eden "+name+"!");
-			// TODO: New Player handling.
-//		}
 	}
 }
