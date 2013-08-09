165fc600cfca5be6abd1bc8c18bc8d3146784458
Matthew Struble <Matt@MayateckInnovations.net>
2013-08-09 11:36:22 -0700

    Added new economy features.

--- src/net/mayateck/OldEdenCore/EconomyHandler.java
+++ src/net/mayateck/OldEdenCore/EconomyHandler.java
@@ -1,5 +1,6 @@
 package net.mayateck.OldEdenCore;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -45,10 +46,90 @@ public class EconomyHandler implements CommandExecutor {
 								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Usage: /money admin set [player] [amount]");
 								return true;
 							}
+						} else if (args[1].equalsIgnoreCase("add")){
+							if (args.length==4){
+								if (plugin.getConfig().contains("players."+args[2]) && s.hasPermission("eden.eco.add")){
+									int funds = plugin.getConfig().getInt("players."+args[2]+".funds") + Integer.parseInt(args[3]);
+									plugin.getConfig().set("players."+args[2]+".funds", funds);
+									plugin.saveConfig();
+									s.sendMessage(OldEdenCore.head+"Success! "+ChatColor.BLUE+args[2]+ChatColor.RESET+" now has "+ChatColor.BLUE+funds+" Bits"+ChatColor.RESET+".");
+									return true;
+								} else {
+									s.sendMessage(OldEdenCore.head+ChatColor.RED+"Player not found or invalid permissions.");
+									return true;
+								}
+							} else {
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Invalid parameter number.");
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Usage: /money admin add [player] [amount]");
+								return true;
+							}
+						} else if (args[1].equalsIgnoreCase("subtract")){
+							if (args.length==4){
+								if (plugin.getConfig().contains("players."+args[2]) && s.hasPermission("eden.eco.add")){
+									int funds = plugin.getConfig().getInt("players."+args[2]+".funds") - Integer.parseInt(args[3]);
+									// WARNING! THIS CAN CREATE NEGATIVE VALUES! CAREFUL WHEN SUBTRACTING!
+									plugin.getConfig().set("players."+args[2]+".funds", funds);
+									plugin.saveConfig();
+									s.sendMessage(OldEdenCore.head+"Success! "+ChatColor.BLUE+args[2]+ChatColor.RESET+" now has "+ChatColor.BLUE+funds+" Bits"+ChatColor.RESET+".");
+									return true;
+								} else {
+									s.sendMessage(OldEdenCore.head+ChatColor.RED+"Player not found or invalid permissions.");
+									return true;
+								}
+							} else {
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Invalid parameter number.");
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Usage: /money admin add [player] [amount]");
+								return true;
+							}
+						} else if (args[1].equalsIgnoreCase("check")){
+							if (args.length==2){
+								if (plugin.getConfig().contains("players."+args[1]) && s.hasPermission("eden.eco.check")){
+									int funds = plugin.getConfig().getInt("players."+args[1]+".funds");
+									s.sendMessage(OldEdenCore.head+ChatColor.BLUE+args[2]+ChatColor.RESET+" has "+ChatColor.BLUE+funds+" Bits"+ChatColor.RESET+".");
+									return true;
+								} else {
+									s.sendMessage(OldEdenCore.head+ChatColor.RED+"Player not found or invalid permissions.");
+									return true;
+								}
+							} else {
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Invalid parameter number.");
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Usage: /money check [player]");
+								return true;
+							}
 						} else {
 							s.sendMessage(OldEdenCore.head+ChatColor.RED+"Unknown sub-command.");
 							return true;
 						}
+					} else if(args[0].equalsIgnoreCase("pay")) {
+						if (args.length==3){
+							if (s.hasPermission("eden.eco.pay")){
+								int senderFunds = plugin.getConfig().getInt("players."+name+".funds");
+								int funds = Integer.parseInt(args[2]);
+								if (funds<=senderFunds){
+									if (plugin.getConfig().contains("players."+args[1])){
+										plugin.getConfig().set("players."+name+".funds", senderFunds-funds);
+										int pFunds = plugin.getConfig().getInt("players."+args[1]+".funds");
+										plugin.getConfig().set("players."+args[1]+".funds", pFunds+funds);
+										plugin.saveConfig();
+										s.sendMessage(OldEdenCore.head+"Success! Sent "+ChatColor.BLUE+funds+" Bits"+ChatColor.RESET+" to "+ChatColor.BLUE+args[1]+ChatColor.RESET+".");
+										Player target = (Bukkit.getServer().getPlayer(args[1]));
+										if (!(target==null)){
+											target.sendMessage(OldEdenCore.head+"You recieved "+ChatColor.BLUE+funds+" Bits"+ChatColor.RESET+" from "+ChatColor.BLUE+name+ChatColor.RESET+".");
+										}
+									} else {
+										s.sendMessage(OldEdenCore.head+"That player doesn't exist.");
+									}
+								} else {
+									s.sendMessage(OldEdenCore.head+"You can't give you you don't have!");
+								}
+							} else {
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"You do not have permission to pay other people.");
+							}
+						} else {
+							s.sendMessage(OldEdenCore.head+ChatColor.RED+"Invalid arguments.");
+							s.sendMessage(OldEdenCore.head+ChatColor.RED+"Usage: /money pay [player] [amount].");
+						}
+						return true;
 					} else {
 						s.sendMessage(OldEdenCore.head+ChatColor.RED+"Unknown sub-command.");
 						return true;
