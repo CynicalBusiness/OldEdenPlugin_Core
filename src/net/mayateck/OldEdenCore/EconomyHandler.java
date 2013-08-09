9507e4de3fc91a53ca14a8c12cedc354efa2e37e
Matthew Struble <Matt@MayateckInnovations.net>
2013-08-09 11:02:36 -0700

    Now properly handles economy.

--- src/net/mayateck/OldEdenCore/EconomyHandler.java
+++ src/net/mayateck/OldEdenCore/EconomyHandler.java
@@ -6,7 +6,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@SuppressWarnings("unused")
 public class EconomyHandler implements CommandExecutor {
 	private OldEdenCore plugin;
 	 
@@ -15,24 +14,44 @@ public class EconomyHandler implements CommandExecutor {
 	}
  
 	public boolean onCommand(CommandSender s, Command cmd, String l, String[] args) {
+		String name = s.getName();
 		if (cmd.getName().equalsIgnoreCase("money")){
 			if (!(s instanceof Player)){
 				s.sendMessage("The economy commands are player-executible only.");
 			} else {
 				if (args.length==0){
-					String name = s.getName();
-					String funds = HTTPGetData.getGeneralData("econ.php", "get=fetch&name="+name);
-					s.sendMessage(funds);
+					int funds = plugin.getConfig().getInt("players."+name+".funds");
+					s.sendMessage(OldEdenCore.serverHead+"You have "+ChatColor.BLUE+funds+" Bits"+ChatColor.RESET+".");
 					return true;
 				} else {
-					if (args[0]=="admin"){
+					if (args[0].equalsIgnoreCase("admin")){
 						if (args.length==1){
 							s.sendMessage(OldEdenCore.head+ChatColor.RED+"Invalid parameter number. Usage: /money admin [args]");
-						} if (args[1]=="set"){
-							if (args.length==2 || args.length==3 || args.length>=5){
-								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Invalid parameter number. Usage: /money admin set [player] [amount]");
-							} 
+							return true;
+						} if (args[1].equalsIgnoreCase("set")){
+							if (args.length==4){
+								if (plugin.getConfig().contains("players."+args[2]) && s.hasPermission("eden.eco.set")){
+									int funds = Integer.parseInt(args[3]);
+									plugin.getConfig().set("players."+args[2]+".funds", funds);
+									plugin.saveConfig();
+									s.sendMessage(OldEdenCore.head+"Success! "+ChatColor.BLUE+args[2]+ChatColor.RESET+" now has "+ChatColor.BLUE+funds+" Bits"+ChatColor.RESET+".");
+									return true;
+								} else {
+									s.sendMessage(OldEdenCore.head+ChatColor.RED+"Player not found or invalid permissions.");
+									return true;
+								}
+							} else {
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Invalid parameter number.");
+								s.sendMessage(OldEdenCore.head+ChatColor.RED+"Usage: /money admin set [player] [amount]");
+								return true;
+							}
+						} else {
+							s.sendMessage(OldEdenCore.head+ChatColor.RED+"Unknown sub-command.");
+							return true;
 						}
+					} else {
+						s.sendMessage(OldEdenCore.head+ChatColor.RED+"Unknown sub-command.");
+						return true;
 					}
 				}
 			}
