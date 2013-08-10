f8bc7103a755f5094c21e06cb6eca1c445ceeba3
Matthew Struble <Matt@MayateckInnovations.net>
2013-08-10 12:28:59 -0700

    Created RegionHandler for later use.

---
+++ src/net/mayateck/OldEdenCore/RegionHandler.java
@@ -0,0 +1,9 @@
+package net.mayateck.OldEdenCore;
+
+@SuppressWarnings("unused")
+public class RegionHandler {
+	private OldEdenCore plugin;
+	public RegionHandler(OldEdenCore plugin) {
+		this.plugin = plugin;
+	}
+}
