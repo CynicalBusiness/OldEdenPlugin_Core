d89e58b38361e3fdaae774e9e37e2de348bc7452
Matthew Struble <Matt@MayateckInnovations.net>
2013-08-11 14:18:01 -0700

    Forgot to implement the event listener.

--- src/net/mayateck/OldEdenCore/RegionHandler.java
+++ src/net/mayateck/OldEdenCore/RegionHandler.java
@@ -1,9 +1,24 @@
 package net.mayateck.OldEdenCore;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
 @SuppressWarnings("unused")
-public class RegionHandler {
+public class RegionHandler implements Listener{
 	private OldEdenCore plugin;
 	public RegionHandler(OldEdenCore plugin) {
 		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent evt){
+		Player plyr = evt.getPlayer();
+		Location loc = plyr.getLocation();
+		double locX = loc.getX();
+		double locZ = loc.getZ();
+		
 	}
 }
